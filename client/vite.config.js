dd952d53da0a8c90c4b1097ff2192c6249a977b0
KS MANOJ KUMAR <ksmk2104@gmail.com>
2025-11-12 05:45:06 +0530

    "Fix : Deplyment Fixes"

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: '../dist', // Move outside client folder for Vercel root deployment
     assetsDir: 'assets',
     sourcemap: false,
     rollupOptions: {
