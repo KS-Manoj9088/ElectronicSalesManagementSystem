3ed2443a5340ff85f4b94b7332dc402339440d1a
KS MANOJ KUMAR <ksmk2104@gmail.com>
2025-11-12 06:23:50 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -51,8 +51,12 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+/*
+//app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   );
 });
+*/
+
+export default app;
